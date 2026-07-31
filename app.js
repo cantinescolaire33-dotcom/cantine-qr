62242ba1754143c3ae12eadc6754fb7a1402c7ab
cantinescolaire33-dotcom <cantinescolaire33@gmail.com>
2026-07-31 21:58:14 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+const result = document.getElementById("result");
 
+function afficherMessage(message, classe = "attente") {
+    result.className = "message " + classe;
+    result.innerHTML = message;
+}
+
+function onScanSuccess(decodedText) {
+
+    afficherMessage(
+        "📷 QR détecté<br><br><strong>" + decodedText + "</strong>",
+        "info"
+    );
+
+}
+
+const scanner = new Html5QrcodeScanner(
+    "reader",
+    {
+        fps: 10,
+        qrbox: 250
+    },
+    false
+);
+
+scanner.render(onScanSuccess);
