ca03d9679b2b2f4b660302e939a6a75a7e2125e1
cantinescolaire33-dotcom <cantinescolaire33@gmail.com>
2026-08-24 19:30:45 +0000

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,9 +1,63 @@
-const SON_SUCCESS = new Audio("sounds/success.mp3");
+let audioContext = null;
+
+function initialiserSon() {
+
+    if (!audioContext) {
+        audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    }
+
+    if (audioContext.state === "suspended") {
+        audioContext.resume();
+    }
+}
+
+
+function jouerBip(frequence, duree) {
+
+    try {
+
+        initialiserSon();
+
+        const oscillateur = audioContext.createOscillator();
+        const gain = audioContext.createGain();
+
+        oscillateur.frequency.value = frequence;
+        oscillateur.type = "sine";
+
+        gain.gain.setValueAtTime(0.15, audioContext.currentTime);
+
+        oscillateur.connect(gain);
+        gain.connect(audioContext.destination);
+
+        oscillateur.start();
+
+        gain.gain.exponentialRampToValueAtTime(
+            0.001,
+            audioContext.currentTime + duree
+        );
+
+        oscillateur.stop(
+            audioContext.currentTime + duree
+        );
+
+    } catch (error) {
+
+        console.log("Son indisponible :", error);
+
+    }
+
+}
+
 
 function jouerSucces() {
-    SON_SUCCESS.currentTime = 0;
 
-    SON_SUCCESS.play().catch(error => {
-        console.log("Son bloqué par le navigateur :", error);
-    });
+    jouerBip(880, 0.20);
+
+}
+
+
+function jouerErreur() {
+
+    jouerBip(300, 0.30);
+
 }
